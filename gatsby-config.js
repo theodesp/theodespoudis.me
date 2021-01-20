75fae03e9054352fa61174ecb6e7c1a79c0342f9
theodesp <theo.despoudis@wpengine.com>
2021-01-20 15:44:58 +0000

    Favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,13 +29,51 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `theodespoudis.me`,
+        short_name: `theodespoudis`,
         start_url: `/`,
         background_color: `#001871`,
         theme_color: `#001871`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon-32x32.png`, // This path is relative to the root of the site.
+        icons: [
+          {
+            "src": "src/images/android-icon-36x36.png",
+            "sizes": "36x36",
+            "type": "image/png",
+            "density": "0.75"
+          },
+          {
+            "src": "src/images/android-icon-48x48.png",
+            "sizes": "48x48",
+            "type": "image/png",
+            "density": "1.0"
+          },
+          {
+            "src": "src/images/android-icon-72x72.png",
+            "sizes": "72x72",
+            "type": "image/png",
+            "density": "1.5"
+          },
+          {
+            "src": "src/images/android-icon-96x96.png",
+            "sizes": "96x96",
+            "type": "image/png",
+            "density": "2.0"
+          },
+          {
+            "src": "src/images/android-icon-144x144.png",
+            "sizes": "144x144",
+            "type": "image/png",
+            "density": "3.0"
+          },
+          {
+            "src": "src/images/android-icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "density": "4.0"
+          }
+        ]
       },
     },
     {
